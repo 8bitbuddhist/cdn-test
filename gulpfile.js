2d6710d23691c3a2e7bcbd5dc4ee2a0c8adad1b7
Andre Newman <andre@andrenewman.net>
2019-02-04 17:51:55 -0500

    Host fix

--- gulpfile.js
+++ gulpfile.js
@@ -56,6 +56,7 @@ var webserver = require('gulp-webserver');
 gulp.task('webserver', function() {
   gulp.src('./')
     .pipe(webserver({
+      host: '0.0.0.0',
       port: 3000,
       fallback: 'index.html',
       directoryListing: false
